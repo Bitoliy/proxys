f7210754d0cce223512f95c3b2d4f5ace47e4015
Bitoliy <86198628+Bitoliy@users.noreply.github.com>
2023-07-18 23:01:23 +0300

    Create install_server_1-2.sh

---
+++ install_server_1-2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Оновлення та установка необхідних пакетів
+apt update && apt upgrade -y
+apt install -y snapd
+reboot
+
+# Після перезавантаження
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+
+# Завантаження і запуск контейнера з проксі
+wget http://fjedi.com/init_server.sh
+chmod +x init_server.sh
+./init_server.sh
+
+# Редагування файлу docker-compose.yml
+nano docker-compose.yml
+
+# Запуск контейнера
+docker-compose up -d
+
+# Відображення журналу контейнера
+docker logs -ft proxy
+
+# Перезавантаження сервера
+reboot
